a365f33e55a329e9c13d2b88a215fbbdfa37d72f
kevin578 <kevinbriggs1@gmail.com>
2016-10-16 10:49:05 -0400

    added arrays

--- quiz2/main.js
+++ quiz2/main.js
@@ -2,9 +2,24 @@
 function validate(){
 
 	var question1 = document.quiz.question1;
-	if (question1.value == "Providence") {
-		document.getElementById("answer").style.visibility = "visible";
+	var question2 = document.quiz.question2;
+	var question3 = document.quiz.question3;
+	var correct = 0;
+	var pictures = ["win.gif", "meh.gif", "lose.gif"];
+	var message = ["Great job", "That's just okay", "You really need to do better"];
 
+	if (question1.value == "Providence") {
+		correct++;
 }
+	if (question2.value == "Hartford") {
+		correct++;
+}	
+	if 
+	
+	document.getElementById("after_submit").style.visibility = "visible";
+
+
+	document.getElementById("answer").innerHTML = "You got " + correct + " correct";
+	document.getElementById("picture").src = "win.gif";
 	}
 	
