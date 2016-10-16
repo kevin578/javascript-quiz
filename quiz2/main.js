14a1518744bf031b6ce79b3594fb338a9cca88f7
kevin578 <kbriggs@aapl000894.local>
2016-10-15 22:40:29 -0400

    in 3 files

---
+++ quiz2/main.js
@@ -0,0 +1,10 @@
+
+function validate(){
+
+	var question1 = document.quiz.question1;
+	if (question1.value == "Providence") {
+		document.getElementById("answer").style.visibility = "visible";
+
+}
+	}
+	
